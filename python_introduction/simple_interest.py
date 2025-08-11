ac873c2b9d521b0595102d51614c8bec0b652c77
Esau Panky Mosia <pankymosia29@gmail.com>
2025-08-11 23:15:39 +0200

    Adds all Python introductory tasks from 0 to 4

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Define the variables for principal, rate, and time
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate the simple interest
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
